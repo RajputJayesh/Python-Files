b71013d084c62a20b484eba6381334bfdbb36e54
JAYESH RAJPUT <65496315+RajputJayesh@users.noreply.github.com>
2023-01-05 18:03:02 +0530

    Delete 4.loop directory

--- 4.loop/print_1_to_n.py
+++
@@ -1,9 +0,0 @@
-'''
-Print Number from 1 to n.
-'''
-
-n=int(input("Enter last number: "))
-i=1
-while i<=n:
-    print(i)
-    i+=1 #i=i+1
--- 4.loop/summation_1to_n.py
+++
@@ -1,8 +0,0 @@
-
-n=int(input("Enter last number in series: "))
-i=1
-s=0
-while(i<=n):
-    s=s+i 
-    i=i+1
-print("summation is: ",s)
