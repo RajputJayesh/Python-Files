beca4baf9c6870d7bfc07f8e9e297f5d3e5e52df
JAYESH RAJPUT <65496315+RajputJayesh@users.noreply.github.com>
2023-01-05 18:05:11 +0530

    Delete 7. module in python directory

--- 7. module in python/string_constant.py
+++
@@ -1,9 +0,0 @@
-'''
-String constants
-
-'''
-import string as s
-
-print(s.ascii_lowercase)
-print(s.ascii_uppercase)
-print(s.ascii_letters)
--- 7. module in python/user_module.py
+++
@@ -1,8 +0,0 @@
-import arithmatic as ar
-
-
-a=int(input("Enter First Number: "))
-b=int(input("Enter Second number: "))
-
-res=ar.addition(a,b)
-print("Addition is: ",res)
