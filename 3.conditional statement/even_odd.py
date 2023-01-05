0d6c40070e0065e3386afad1ff71f18c46b10bb3
JAYESH RAJPUT <65496315+RajputJayesh@users.noreply.github.com>
2023-01-05 18:02:52 +0530

    Delete 3.conditional statement directory

--- 3.conditional statement/even_odd.py
+++
@@ -1,7 +0,0 @@
-print("Enter number:")
-n=int(input())
-r=n%2
-if r==0:
-    print("Even Number")
-else:
-    print("Odd Number")
--- 3.conditional statement/sumation.py
+++
@@ -1,8 +0,0 @@
-
-n=int(input("Enter the number: "))
-i=1
-s=1
-while(i<=n):
-    s=s+i
-    i=i+1
-    print("Summation is: ",s)
